1b945f7ae01a65945b0c32f9b77df277029d86b4
ugovaretto <uvaretto@cscs.ch>
2013-08-09 19:09:07 +0200

    Lambda+closures

---
+++ lambda/lambda1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <functional>
+
+#if __cplusplus != 201103
+#error "Not a C++11 compliant compiler\n";
+#endif
+
+std::function< int () > MakeFunction() {
+	return [] {
+	    return __cplusplus;
+	};
+}
+
+std::function< int () > MakeCounter1(int c) {
+    return [=]() mutable { return c++; };
+}
+
+std::function< int () > MakeCounter2() {
+    int i = 0;
+    return [=]() mutable { return i++; };
+}
+
+int main(int, char**) {
+    std::cout << MakeFunction()() << std::endl;	
+    auto counter = MakeCounter1(2);
+    std::cout << counter() << ' ' << counter() << std::endl;
+    int v[] = {1, 2, 3, 4, 5};
+    for(auto i: v) { std::cout << i;}
+    std::cout << std::endl;
+    return 0;
+}
+
