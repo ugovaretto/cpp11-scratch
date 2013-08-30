ce42ed05248eb829d6e96809dee584ab00c79db8
ugovaretto <uvaretto@cscs.ch>
2013-08-31 00:07:30 +0200

    Added policy-based example

--- blog/res-handler-1.cpp
+++ blog/res-handler-1.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 //------------------------------------------------------------------------------
-#if __cplusplus < 201103L
+#if __cplusplus < 201103L  // C++ 98
 template <typename T>
 class MemoryHandler {
 private:
@@ -38,13 +38,9 @@ private:
 private:
     T* ptr_;
 };
-#else
+#else // C++ 11
 template <typename T>
 class MemoryHandler {
-private:
-    struct Proxy {
-        T* ptr;  
-    };
 public:
     MemoryHandler(MemoryHandler&& mh) : ptr_(mh.ptr_) {
         mh.ptr_ = 0;
@@ -71,20 +67,46 @@ private:
 private:
     T* ptr_;
 };
+// generic handler
+template <typename T,
+          typename ValidationPolicy,
+          typename ResetPolicy,
+          typename ReleasePolicy>
+class Handler {
+public:
+    Handler(Handler&& h) : res_(h.res_) {
+        ReleasePolicy::Reset(h.res_);
+    }
+    Handler(const Handler&) = delete;
+    explicit Handler(T res = T()) : res_(res) {
+        ReleasePolicy::Reset(res_);
+    }
+    T& get() { return res_; }
+    const T& get() const { return res_; }
+    ~Handler() { 
+        if(ValidationPolicy::Valid(res_)) {
+            ReleasePolicy::Release(res_);
+        }
+    }
+    Handler& operator=(Handler&& h) {
+        Handler(h).Swap(*this);
+        return *this;
+    }
+    Handler& operator=(Handler& h) {
+        Handler(std::move(h)).Swap(*this);
+        return *this;
+    }
+    Handler& operator=(const Handler&) = delete;
+private:
+    void Swap(Handler& h) {
+        std::swap(h.res_, res_);
+    } 
+private:
+    T res_;
+};
 #endif
 
 //------------------------------------------------------------------------------
-struct Printer {
-    Printer(const char*) {}
-    void Print(const char*) {}
-};
-
-void PrintCppStd() {
-  MemoryHandler< Printer > mh(new Printer("remote printer 1"));
-  Printer* p = mh.ptr();
-  p->Print("./ISO_IEC_14882-2011.pdf");  
-} //resource automatically destroyed by MemoryHandler destructor
-
 MemoryHandler< int > MemHandlerFactory(int i) {
     return MemoryHandler< int >(new int (i));
 }
@@ -124,7 +146,6 @@ void Test() {
 
 //------------------------------------------------------------------------------
 int main(int, char**) {
-    PrintCppStd();
     Test();
     return 0;
 }
