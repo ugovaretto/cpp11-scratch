35b812afe5385399980e665c28fbd55132475491
ugo <uvaretto@cscs.ch>
2013-09-06 23:06:41 +0200

    Finished tuple implementation

--- tuple.cpp
+++ tuple.cpp
@@ -1,20 +1,7 @@
 
+#include <cassert>
 #include <iostream>
-// template < int I, typename T >
-// class TupleStorage {
-// public:
-//     TupleStorage(const T& v) : v_(v) {}
-//     const T& get() const { return v_; }
-//     T& get() { return v_; }
-// private:
-//     T v_;        
-// };
 
-// struct Empty {};
-
-// template < int I >
-// class TupleStorage< I, Empty > {    
-// };
 //-----------------------------------------------------------------------------
 struct Nulltype {};
 
@@ -31,7 +18,7 @@ struct First {
 
 template < typename ListT >
 struct Rest {
-    typedef typename ListT::Rest Type;
+    typedef typename ListT::Tail Type;
 };
 
 template < typename L >
@@ -53,22 +40,27 @@ struct Cons {
 
 template < int I, int N, typename ListT >
 struct GetTypeHelper {
-    typedef typename GetTypeHelper< I, N - 1, ListT >::Type Type;
+    typedef typename GetTypeHelper< I, N + 1, typename ListT::Tail >::Type Type;
+    enum {t=false, n = N, i = I};
     
 };
 
 template < int I, typename ListT >
 struct GetTypeHelper< I, I, ListT > {
     typedef typename ListT::Head Type;
+    enum{t=true, i = I};
     
 };
 
+// template < int N, typename ListT >
+// struct GetTypeHelper< 0, N, ListT > {
+//     typedef typename ListT::Head Type;
+// };
+
 template < int I, typename ListT >
 struct GetType {
-    enum {idx = I,
-          bound = Size<ListT>::size - 1};
-    typedef typename GetTypeHelper< idx,
-                                    bound, 
+    typedef typename GetTypeHelper< I,
+                                    0, 
                                     ListT >::Type Type;    
 };
 
@@ -80,55 +72,86 @@ struct TypeAssert {
 template < typename T >
 struct TypeAssert< T, T> {
     enum {Equal = 1};
-};    
+};
+//-----------------------------------------------------------------------------
 
-// template < typename T0,
-//            typename T1 = Empty,
-//            typename T2 = Empty> 
-// class Tuple : TupleStorage< 0, T0 >,
-//               TupleStorage< 1, T1 >,
-//               TupleStorage< 2, T2 > {
+template < int I, typename T >
+class TupleStorage {
+public:
+    typedef T Type;
+    TupleStorage(const T& v) : v_(v) {}
+    const T& get() const { return v_; }
+    T& get() { return v_; }
+private:
+    T v_;        
+};
 
-// public:
-//     Tuple(const T0& v0, const T1& v1 = T1(), const T2& v2 = T2()) :
-//         TupleStorage< 0, T0 >(v0),
-//         TupleStorage< 1, T1 >(v1),
-//         TupleStorage< 2, T2 >(v2) {}
-//     template < int i >
-//     typename Type< i >::type get() {
-//         return typename TupleStorage< i, typename 
-//         Type< i >::type >::get();
-//     }    
-// };
+struct Empty {};
 
+template < int I >
+class TupleStorage< I, Empty > {
+public:
+    typedef Empty Type;
+    TupleStorage(const Empty&) {}
+};
 
-// template < typename T0, typename T1, typename T2 >
-// struct Tuple< T0, T1, T2 >::Type<0> {
-//     typedef T0 type;
-// };
+template < typename T0,
+           typename T1 = Empty,
+           typename T2 = Empty,
+           typename T3 = Empty > 
+class Tuple : TupleStorage< 0, T0 >,
+              TupleStorage< 1, T1 >,
+              TupleStorage< 2, T2 >,
+              TupleStorage< 3, T3 > {
+typedef TupleStorage< 0, T0 > S0;
+typedef TupleStorage< 1, T1 > S1;
+typedef TupleStorage< 2, T2 > S2;
+typedef TupleStorage< 3, T3 > S2;
+typedef 
+typename Cons< S0, 
+    typename Cons< S1,
+        typename Cons< S2,
+            typename Cons< S3 >
+::Type >
+    ::Type >
+        ::Type >
+            ::Type Types;
+public:
+    Tuple(const T0& v0,
+          const T1& v1 = T1(),
+          const T2& v2 = T2(),
+          const T3& v3 = T3()) :
+        TupleStorage< 0, T0 >(v0),
+        TupleStorage< 1, T1 >(v1),
+        TupleStorage< 2, T2 >(v2),
+        TupleStorage< 3, T3 >(v3) {}
+    template < int i >
+    typename GetType< i, Types >::Type::Type get() {
+        return GetType< i, Types >::Type::get();
+    }    
+};
 
-// template < typename T0, typename T1, typename T2 >
-// template <>
-// struct Tuple< T0, T1, T2 >::Type<1> {
-//     typedef T1 type;
-// };
-
-// template < typename T0, typename T1, typename T2 >
-// template <>
-// struct Tuple< T0, T1, T2 >::Type<2> {
-//     typedef T2 type;
-// };
-
+//-----------------------------------------------------------------------------
 
 int main(int, char**) {
-    typedef Cons< float, Cons<int>::Type >::Type T;
-    std::cout << Size<T>::size << std::endl;
-    GetType< 0, T >::Type t;
-    std::cout << TypeAssert< int, float >::Equal << std::endl;
-    std::cout << TypeAssert< int, int >::Equal << std::endl;
-    std::cout << TypeAssert< GetType< 1, T >::Type, int >::Equal << std::endl;
-    //Tuple< int, float > t(1, 2.0f);
-    //std::cout << t.get<0>() << ' ' << t.get<1>() << std::endl;
+    typedef Cons< float, Cons<int, Cons<char>::Type >::Type >::Type List;
+    assert(Size<List>::size == 3);
+    typedef GetType< 0, List >::Type Type0;
+    typedef GetType< 1, List >::Type Type1;
+    typedef GetType< 2, List >::Type Type2;
+    
+    //Without the double parentheses in assert with clang llvm 3.2:
+    //../tuple.cpp:133:40: error: too many arguments provided to function-like
+    // macro invocation
+    //assert(typename TypeAssert< Type0, float >::Equal == 1);
+    //                                   ^
+    //../tuple.cpp:133:5: error: use of undeclared identifier 'assert'
+    assert((TypeAssert< Type0, float >::Equal == 1));
+    assert((TypeAssert< Type1, int>::Equal == 1));
+    assert((TypeAssert< Type2, char>::Equal == 1));
+    Tuple< int, float > t(1, 2.0f);
+    assert(t.get<0>() == 1);
+    assert(t.get<1>() == 2.0f);
     return 0;
 }
 
