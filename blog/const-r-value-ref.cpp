59a39b538e8a76dc94b4b2d0a8094a79b23ded75
ugovaretto <uvaretto@cscs.ch>
2013-09-09 12:05:00 +0200

    Added forward/move and const r-value references sample code

---
+++ blog/const-r-value-ref.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//const T&& - a use case: when keeping references we do not want to accept
+//references to temporary objects
+void bar(const int&& i) {
+    std::cout << "Noooooooo\n";
+};
+void bar(const int& i) {
+    std::cout << "OK\n";
+};
+int main(int, char**) {
+    int i = int();
+    bar(i);
+    bar(1);
+    bar(([](){return 1;})());
+    return 0;
+}
