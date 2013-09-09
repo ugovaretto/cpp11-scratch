111fab723e96da16cb41f1a42858f23df1348fc2
ugovaretto <uvaretto@cscs.ch>
2013-09-09 12:23:18 +0200

    ...

--- blog/const-r-value-ref.cpp
+++ blog/const-r-value-ref.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 //const T&& - a use case: when keeping references we do not want to accept
 //references to temporary objects
-void bar(const int&& i) {
+void bar(const int&&) {
     std::cout << "Noooooooo\n";
 };
-void bar(const int& i) {
+void bar(int&) {
     std::cout << "OK\n";
 };
+void bar(const int&) {
+    std::cout << "OK\n";
+}
 int main(int, char**) {
     int i = int();
     bar(i);
