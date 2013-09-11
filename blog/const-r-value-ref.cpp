3e9a1314a81e22a6dcf865a83f000118784f93e0
ugovaretto <uvaretto@cscs.ch>
2013-09-11 16:40:27 +0200

    Update

--- blog/const-r-value-ref.cpp
+++ blog/const-r-value-ref.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 //const T&& - a use case: when keeping references we do not want to accept
 //references to temporary objects
+#ifdef RVALREF
 void bar(const int&&) {
-    std::cout << "Noooooooo\n";
+    std::cout << "&& - !!!\n";
 };
+#endif
 void bar(int&) {
-    std::cout << "OK\n";
+    std::cout << "&\n";
 };
 void bar(const int&) {
-    std::cout << "OK\n";
+    std::cout << "const &\n";
 }
 int main(int, char**) {
     int i = int();
-    bar(i);
-    bar(1);
-    bar(([]{return 1;})());
+    bar(i); // calls &
+    bar(1); // calls && (would call const & if && overload not present)
+    bar(([]{return 1;})()); // calls && (would call const & if &&
+                            //   overload not present)
     return 0;
 }
