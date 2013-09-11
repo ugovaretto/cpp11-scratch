e2b6beabcdaf317eed0f9723a45b5a794d8a57f7
ugovaretto <uvaretto@cscs.ch>
2013-09-11 15:47:40 +0200

    Updated forward iteration

--- blog/forward-template-iteration.cpp
+++ blog/forward-template-iteration.cpp
@@ -7,25 +7,25 @@ struct ReversePrinter {
         //no idea at this point in the code about the actual number of
         //iterations, would need to have a separate int template parameter
         //holding the number of iterations
-        for(int t = 0; t != N; ++t) std::cout << "  ";
+        for(int t = 0; t != N - 1; ++t) std::cout << "  ";
         std::cout << "Iteration " << N << '\n';
         ReversePrinter< N - 1 >::Print();
     }    
 };
 
 template <>
-struct ReversePrinter< 0 > {
+struct ReversePrinter< 1 > {
     static void Print() {
-        std::cout << "Iteration 0" << std::endl;
+        std::cout << "Iteration 1" << std::endl;
     }
 };
 
 //------------------------------------------------------------------------------
-template < int N, int Count = 0 > // add a counter as the last parameter
-struct Printer {                  // default initialized to zero, client code
+template < int N, int Count = 1 > // add a counter as the last parameter
+struct Printer {                  // default initialized to one, client code
     static void Print() {         // is free to change this to select a
-        for(int t = 0; t != Count; ++t) std::cout << "  ";// different offset
-        std::cout << "Iteration " << Count << '\n'; 
+        for(int t = 0; t != Count - 1; ++t) std::cout << "  ";// different
+        std::cout << "Iteration " << Count << '\n';           // offset 
         Printer< N, Count + 1 >::Print();
     }
 };
@@ -33,18 +33,17 @@ struct Printer {                  // default initialized to zero, client code
 template < int N >
 struct Printer< N, N > {
     static void Print() {
-        for(int t = 0; t != N; ++t) std::cout << "  ";
+        for(int t = 0; t != N - 1; ++t) std::cout << "  ";
         std::cout << "Iteration " << N << std::endl;
     }
 };
-
 
 //------------------------------------------------------------------------------
 int main(int, char**) {
     std::cout << "Standard reverse iterator:\n";
     ReversePrinter< 3 >::Print();
     std::cout << '\n';
-    std::cout << "In order iterator:\n";
+    std::cout << "Forward iterator:\n";
     Printer< 3 >::Print();
     return 0;
 }
