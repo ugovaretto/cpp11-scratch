ec03a1057ae59408199baf24bd15ac52e36f3564
ugo <uvaretto@cscs.ch>
2013-09-22 19:29:17 +0200

    Added task-based executor

--- executor.cpp
+++ executor.cpp
@@ -2,45 +2,12 @@
 #include <condition_variable>
 #include <thread>
 #include <chrono>
- 
-std::condition_variable cv;
-std::mutex cv_m; // This mutex is used for three purposes:
-                 // 1) to synchronize accesses to i
-                 // 2) to synchronize accesses to std::cerr
-                 // 3) for the condition variable cv
-int i = 0;
- 
-void waits()
-{
-    std::unique_lock<std::mutex> lk(cv_m);
-    std::cerr << "Waiting... \n";
-std::this_thread::sleep_for(std::chrono::seconds(10));
-    cv.wait(lk, []{return i == 1;});
-    
-    std::cerr << "...finished waiting. i == 1\n";
+#include <future>
+#include <type_traits>
+#include <utility>
+#include <deque>
 
-    
-}
- 
-void signals()
-{
-       
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    {
-        std::lock_guard<std::mutex> lk(cv_m);
-        std::cerr << "Notifying...\n";
-    }
-    cv.notify_all();
-    
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-     i = 1;
-    {
-        std::lock_guard<std::mutex> lk(cv_m);
-        std::cerr << "Notifying again...\n";
-    }
-    cv.notify_all();
-}
-
+//------------------------------------------------------------------------------
 template < typename T >
 SyncQueue {
 public:
@@ -57,19 +24,19 @@ public:
         return e;
     }  
 private:
-    bool Remove(const T& e) {
-        //if used to signal thread termination return false
-        return true;
-    }   
-
+    template < typename T >
+    bool Remove(const T& e) { return true; } 
 private:
     std::deque< T > queue_;
     std::mutex mutex_;
     std::condition_variable cond_;
 };
 
+//------------------------------------------------------------------------------
 struct ICaller {    
-virtual ~Caller() {}
+    virtual bool Empty() const = 0;    
+    virtual void Invoke() = 0;    
+    virtual ~Caller() {}
 };
 
 
@@ -91,19 +58,81 @@ private:
     std::function< ResultType () > f_; 
 }; 
 
-class Executor {
+template <>
+class Caller<void> : ICaller {
+public:
+    template < typename U, typename... Args >    
+    Caller(U f, Args...args) : f_(std::bind(f, args...)), empty_(false) {}
+    Caller() : empty_(true) {}
+    std::future< void > GetFuture() {
+        return p_.get_future();
+    }
+    void Invoke() {
+        try {
+            f_();
+            p_.set_value();
+        } catch(...) {
+            p_.set_exception(std::current_exception());
+        }
+    }
+    bool Empty() const { return empty_; }
+private:
+    std::promise< void > p_;
+    std::function< void () > f_; 
+    bool empty_;
+}; 
 
+bool Remove(ICaller* c) { return !c->Empty(); }
+
+//------------------------------------------------------------------------------
+class Executor {
+    typedef SyncQueue< ICaller* > Queue;
+    typedef std::vector< std::thread > Threads;
+public:
+    Executor(int numthreads = std::thread::hardware_concurrency()) {
+        for(int t = 0; t != numthreads; ++t) {
+            threads.push_back(std::thread( [this] {
+                while(true) {
+                    ICaller* c = queue_.Pop();
+                    if(c->Empty()) {
+                        delete c;
+                        break;
+                    }
+                    c->Invoke();
+                    delete c;
+                }
+            });    
+        }    
+    }
+    template < typename F, typename... Args > 
+    auto operator()(F f, Args... args) -> std::future< decltype(f(args...)) >{
+        typedef decltype(f(args...)) ResultType
+        Caller< ResultType >* c = new Caller< ResultType >(f, args...);
+        queue_.Push(c);
+        return c->GetFuture()
+    }
+    void operator()() {
+        queue_.Push(new Caller<void>);
+    }
+private:
+    Queue queue_;
+    Threads threads_;
 };
+
+//------------------------------------------------------------------------------
+int sum(int start, int end, int step = 1) {
+    int ret = start;
+    for(;start != end + 1; ++start) {
+        ret += ste;
+    }
+}
  
-int main()
-{
-    auto exec = MakeExecutor(4);
-    std::future< int > exec<int>(counter, 10);
-    std::future< int >
-    std::thread t1(waits), t2(waits), t3(waits), t4(signals);
-    t1.join(); 
-    t2.join(); 
-    t3.join();
-    t4.join();
-    return 0;
+int main(int, char**) {
+    Executor exec(3);
+    auto f1 = exec(sum, 1, 10);
+    auto f2 = exec(sum, 10, 100);
+    auto f3 = exec(sum, 100, 1000, 100);
+    exec();
+    std::cout << f1.get() << ' ' << f2.get() << ' ' << f3.get() << std::endl;
+    return 0;    
 }
