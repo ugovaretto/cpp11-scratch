0952ca5d496c984373b4727d5865a8f543c80f94
ugo <uvaretto@cscs.ch>
2013-09-23 00:14:51 +0200

    Fixed termination

--- executor.cpp
+++ executor.cpp
@@ -7,31 +7,24 @@
 #include <utility>
 #include <deque>
 #include <vector>
+#include <stdexcept>
 
 //------------------------------------------------------------------------------
 template < typename T >
-class 
-SyncQueue {
+class SyncQueue {
 public:
     void Push(const T& e) {
         std::lock_guard< std::mutex > guard(mutex_);
         queue_.push_front(e);
         cond_.notify_one();
-        cond_.notify_one();
-        cond_.notify_one();
     }
     T Pop() {
         std::unique_lock< std::mutex > lock(mutex_);
-        cond_
-        .wait(lock, [this]{ return !queue_.empty();});
-        T e = std::move(queue_.back());
-        if(Remove(e)) queue_.pop_back();
+        cond_.wait(lock, [this]{ return !queue_.empty();});
+        T e = queue_.back();
+        queue_.pop_back();
         return e;
     }  
-private:
-    template < typename E >
-    bool Remove(const E
-        & e) { return true; } 
 private:
     std::deque< T > queue_;
     std::mutex mutex_;
@@ -72,8 +65,7 @@ private:
 
 
 template <>
-class Caller<void> : public 
-ICaller {
+class Caller<void> : public ICaller {
 public:
     template < typename U, typename... Args >    
     Caller(U f, Args...args) : f_(std::bind(f, args...)), empty_(false) {}
@@ -105,52 +97,52 @@ class Executor {
 public:
     Executor(int numthreads = std::thread::hardware_concurrency()) {
         for(int t = 0; t != numthreads; ++t) {
-            threads_.push_back(std::thread( [this] {
+            threads_.push_back(std::move(std::thread( [this] {
                 while(true) {
                     ICaller* c = queue_.Pop();
                     if(c->Empty()) {
-                        std::cout << "terminating" << std::endl;
                         break;
                     }
-                    c->Invoke();
+                    c->Invoke(); 
                     delete c;
                 }
-            }));    
+            })));    
         }    
     }
     template < typename F, typename... Args > 
-    auto operator()(F f, Args... args) -> std::future< decltype(f(args...)) >{
+    auto operator()(F f, Args... args) -> std::future< decltype(f(args...)) > {
+        if(threads_.empty()) throw std::logic_error("No active threads");
         typedef decltype(f(args...)) ResultType;
         Caller< ResultType >* c = new Caller< ResultType >(f, args...);
         std::future< ResultType > ft = c->GetFuture();
         queue_.Push(c);
         return ft;
     }
-    void operator()() {
-        queue_.Push(new Caller<void>);
-        //std::for_each(threads_.begin(), threads_.end(), [](std::thread& t)
-        //    {t.join();});
+    void Terminate() {
+        for(int t = 0; t != threads_.size(); ++t) queue_.Push(new Caller<void>); 
+        std::for_each(threads_.begin(), threads_.end(), [](std::thread& t)
+                                                            {t.join();});
+        threads_.clear();
     }
+    ~Executor() { Terminate(); }
 private:
     Queue queue_;
     Threads threads_;
 };
 
 //------------------------------------------------------------------------------
-int sum(int start, int end, int step = 1) {
+int sum(int start, int end, int step) {
     for(;start < end; start += step)
         ;
-
-        return start;
+    return start;
 }
 
 int main(int, char**) {
-    Executor exec(3);
-    auto f1 = exec(sum, 1, 10, 1);
+    Executor exec(2);
+    using namespace std::placeholders;
+    auto f1 = exec(std::bind(sum, _1, _2, 1), 1, 10);
     auto f2 = exec(sum, 10, 100, 1);
     auto f3 = exec(sum, 100, 1000, 100);
-    exec();
     std::cout << f1.get() << ' ' << f2.get() << ' ' << f3.get() << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(10));
     return 0;    
 }
