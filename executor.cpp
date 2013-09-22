d7c75800090862fd3fce21dfeb7776f33115485e
ugo <uvaretto@cscs.ch>
2013-09-22 23:06:38 +0200

    ...

--- executor.cpp
+++ executor.cpp
@@ -6,26 +6,32 @@
 #include <type_traits>
 #include <utility>
 #include <deque>
+#include <vector>
 
 //------------------------------------------------------------------------------
 template < typename T >
+class 
 SyncQueue {
 public:
-    void Push(T&& e) {
+    void Push(const T& e) {
         std::lock_guard< std::mutex > guard(mutex_);
-        queue_.push_front(std::move(e));
-        cond.notify_one();
+        queue_.push_front(e);
+        cond_.notify_one();
+        cond_.notify_one();
+        cond_.notify_one();
     }
     T Pop() {
         std::unique_lock< std::mutex > lock(mutex_);
-        cond.wait(mutex_, [this]{ return !queue_.empty();});
+        cond_
+        .wait(lock, [this]{ return !queue_.empty();});
         T e = std::move(queue_.back());
         if(Remove(e)) queue_.pop_back();
         return e;
     }  
 private:
-    template < typename T >
-    bool Remove(const T& e) { return true; } 
+    template < typename E >
+    bool Remove(const E
+        & e) { return true; } 
 private:
     std::deque< T > queue_;
     std::mutex mutex_;
@@ -36,30 +42,38 @@ private:
 struct ICaller {    
     virtual bool Empty() const = 0;    
     virtual void Invoke() = 0;    
-    virtual ~Caller() {}
+    virtual ~ICaller() {}
 };
 
 
-template < typename F  >
-class Caller {
-    typedef std::result_of< F >::type ResultType;
-public:    
-    Caller(F f, Args...args) : f_(std::bind(f, args)) {}
+template < typename ResultType >
+class Caller : public ICaller {
+public:
+    template < typename U, typename... Args >    
+    Caller(U f, Args...args) : f_(std::bind(f, args...)), empty_(false) {}
+    Caller() : empty_(true) {}
+    std::future< ResultType > GetFuture() {
+        return p_.get_future();
+    }
     void Invoke() {
         try {
-            ResultType r = f_();
+            ResultType r = ResultType(f_());
             p_.set_value(r);
-        } catch(const std::exception& e) {
-            p_.set_exception(e);
+        } catch(...) {
+            p_.set_exception(std::current_exception());
         }
     }
+    bool Empty() const { return empty_; }
 private:
     std::promise< ResultType > p_;
     std::function< ResultType () > f_; 
+    bool empty_;
 }; 
 
+
 template <>
-class Caller<void> : ICaller {
+class Caller<void> : public 
+ICaller {
 public:
     template < typename U, typename... Args >    
     Caller(U f, Args...args) : f_(std::bind(f, args...)), empty_(false) {}
@@ -91,28 +105,31 @@ class Executor {
 public:
     Executor(int numthreads = std::thread::hardware_concurrency()) {
         for(int t = 0; t != numthreads; ++t) {
-            threads.push_back(std::thread( [this] {
+            threads_.push_back(std::thread( [this] {
                 while(true) {
                     ICaller* c = queue_.Pop();
                     if(c->Empty()) {
-                        delete c;
+                        std::cout << "terminating" << std::endl;
                         break;
                     }
                     c->Invoke();
                     delete c;
                 }
-            });    
+            }));    
         }    
     }
     template < typename F, typename... Args > 
     auto operator()(F f, Args... args) -> std::future< decltype(f(args...)) >{
-        typedef decltype(f(args...)) ResultType
+        typedef decltype(f(args...)) ResultType;
         Caller< ResultType >* c = new Caller< ResultType >(f, args...);
+        std::future< ResultType > ft = c->GetFuture();
         queue_.Push(c);
-        return c->GetFuture()
+        return ft;
     }
     void operator()() {
         queue_.Push(new Caller<void>);
+        //std::for_each(threads_.begin(), threads_.end(), [](std::thread& t)
+        //    {t.join();});
     }
 private:
     Queue queue_;
@@ -121,18 +138,19 @@ private:
 
 //------------------------------------------------------------------------------
 int sum(int start, int end, int step = 1) {
-    int ret = start;
-    for(;start != end + 1; ++start) {
-        ret += ste;
-    }
+    for(;start < end; start += step)
+        ;
+
+        return start;
 }
- 
+
 int main(int, char**) {
     Executor exec(3);
-    auto f1 = exec(sum, 1, 10);
-    auto f2 = exec(sum, 10, 100);
+    auto f1 = exec(sum, 1, 10, 1);
+    auto f2 = exec(sum, 10, 100, 1);
     auto f3 = exec(sum, 100, 1000, 100);
     exec();
     std::cout << f1.get() << ' ' << f2.get() << ' ' << f3.get() << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(10));
     return 0;    
 }
