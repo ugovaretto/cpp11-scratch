4404c7536ce2fe1d1b2fa125f0f3d3f6af9e679a
ugo <uvaretto@cscs.ch>
2013-10-06 00:46:48 +0200

    Changed int to template T

--- blog/const-r-value-ref.cpp
+++ blog/const-r-value-ref.cpp
@@ -1,23 +1,54 @@
+#include <functional>
 #include <iostream>
 //const T&& - a use case: when keeping references we do not want to accept
 //references to temporary objects
-#ifdef RVALREF
-void bar(const int&&) {
+//#ifdef CRVALREF
+template < typename T >
+void bar(const T&&) {
+    std::cout << "const && - !!!\n";
+};
+//#endif
+//#ifdef RVALREF
+template < typename T >
+void bar(T&&) {
     std::cout << "&& - !!!\n";
 };
-#endif
-void bar(int&) {
+//#endif
+template < typename T >
+void bar(T&) {
     std::cout << "&\n";
 };
-void bar(const int&) {
+template < typename T >
+void bar(const T&) {
     std::cout << "const &\n";
 }
+
+template < typename T >
+void f(const T&& i) {
+  bar(i);
+}
+
+template < typename T >
+const T foo(const T& v) {return v; }
+
+class C {
+public:
+ C() : i(1) {}
+private:
+int i;
+};
+
 int main(int, char**) {
     int i = int();
-    bar(i); // calls &; removing the int& overload results in a compilation
+    ++i = 2;
+    bar(i++); // calls &; removing the int& overload results in a compilation
             // error: use std::move to call the && overload
-    bar(1); // calls && (would call const & if && overload not present)
+    bar(foo(C())); // calls && (would call const & if && overload not present)
     bar(([]{return 1;})()); // calls && (would call const & if &&
                             //   overload not present)
+    f(2);
+    bar(2);
+    const int&& r = 2;
+    bar(r);
     return 0;
 }
