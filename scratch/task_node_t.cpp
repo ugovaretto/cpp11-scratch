d361164d1d5133c98e00a02cd9a3a48b252bc6a0
ugo <uvaretto@cscs.ch>
2013-11-03 17:17:23 +0100

    ...

--- scratch/task_node_t.cpp
+++ scratch/task_node_t.cpp
@@ -5,6 +5,8 @@ task nodes shall have
 - message reporter read message string
 - status reported: executing/error
 
+- add optional allocator for all types
+
 task_node_t tn;
 tn.action = make_action_wrapper(tn.promise, fun, param1, param2,...);
 
