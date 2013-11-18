22314d38c84b31a2a03d5288268d2df2efed63fb
ugovaretto <uvaretto@cscs.ch>
2013-11-18 17:17:15 +0100

    Changed code that caused clang to crash

--- blog/simple-function.cpp
+++ blog/simple-function.cpp
@@ -1,8 +1,13 @@
 //Author; Ugo Varetto
-//simple implementation of std::function-like class, clang compiler >= 3.3
-//crashes! not tried with < 3.3, builds fine with g++ 4.8.1
+//simple implementation of std::function-like class using lambdas and unions
+//TODO:
+// - void,
+// - forward
+// - const for functor objects
+// - copy, assignment and move
 #include <cassert>
 #include <iostream>
+#include <vector>
 //==============================================================================
 //generic
 template < typename > struct Fun {};
@@ -13,60 +18,81 @@ struct Fun< R (ArgTypes...) >  {
     Fun(R (*f)(ArgTypes...));
     template < typename T > Fun(R (T::*f)(ArgTypes...) );
     template < typename T > Fun(R (T::*f)(ArgTypes...) const);
+    template < typename T > Fun(const T& f);
     R operator()(ArgTypes...args);
     template < typename T > R operator()(T& obj, ArgTypes...args);
     template < typename T > R operator()(const T& obj, ArgTypes...args);
     union {
         R(*free_)(ArgTypes...);
         R(*method_)(void* ,  R (Object::*)(ArgTypes...), ArgTypes...);
-        R(*cmethod_)(const void* , R (Object::*)(ArgTypes...) const, ArgTypes...);
+        R(*cmethod_)(const void* , R (Object::*)(ArgTypes...) const,
+                     ArgTypes...);
     } f_;
     union {
         R(Object::*method_)(ArgTypes...);
         R(Object::*cmethod_)(ArgTypes...) const;
+        R(*call_)(Fun*, ArgTypes...);
+        R(*obj_)(Fun*, ArgTypes...);
     } m_;
+    std::vector< char > buf_;
+    ~Fun() { //todo add lambda to invoke destructor on functor stored into buf_
+    }
 };
 
 //non-void return type
 template < typename R, typename...ArgTypes >
 Fun< R (ArgTypes...) >::Fun(R (*f)(ArgTypes...)) { 
-    std::cout << "R (*f)(ArgTypes...)" << std::endl;
-    f_.free_ = f;
+    f_.free_ = f; 
+    using T = R (*)(Fun*, ArgTypes...);
+    m_.call_ = (T) ([](Fun* f, ArgTypes...args) {
+         return f->f_.free_(args...);
+    });
 }
 
 template < typename R, typename...ArgTypes >
 template < typename T >
 Fun< R(ArgTypes...) >::Fun(R (T::*f) (ArgTypes...) ) {
-    std::cout << "R (T::*f)(ArgTypes...)" << std::endl;
     using F = R (*)(void*, R (Object::* )(ArgTypes...), ArgTypes...);
     using OM = R (Object::*)(ArgTypes...);
     f_.method_ = (F) ([](void* obj, OM m, ArgTypes...args) {
-            using M = R (T::*)(ArgTypes...);
-            M method = (M) m;
-            return (static_cast< T* >(obj)->*method)(args...);
-        });
+             using M = R (T::*)(ArgTypes...);
+             M method = reinterpret_cast< M >(m);
+             return (static_cast< T* >(obj)->*method)(args...);
+             return R();
+         });
     using OM = R (Object::*)(ArgTypes...);
-    m_.method_ = (OM) f; 
+    m_.method_ = reinterpret_cast< OM >(f); 
 }
 
 template < typename R, typename...ArgTypes >
 template < typename T >
 Fun< R(ArgTypes...) >::Fun(R (T::*f) (ArgTypes...) const) {
-    std::cout << "R (T::*f)(ArgTypes...) const" << std::endl;
-    using F = R (*)(const void*, R (Object::* )(ArgTypes...) const, ArgTypes...);
+    using F = R (*)(const void*, R (Object::* )(ArgTypes...) const,
+              ArgTypes...);
     using OM = R (Object::*)(ArgTypes...) const;
     f_.cmethod_ = (F) ([](const void* obj, OM m, ArgTypes...args) {
             using M = R (T::*)(ArgTypes...) const;
-            M method = (M) m;
+            M method = reinterpret_cast< M >(m);
             return (static_cast< const T* >(obj)->*method)(args...);
         });
     using OM = R (Object::*)(ArgTypes...) const;
-    m_.cmethod_ = (OM) f; 
+    m_.cmethod_ = reinterpret_cast< OM >(f); 
+}
+
+template < typename R, typename...ArgTypes >
+template < typename F >
+Fun< R (ArgTypes...) >::Fun(const F& f) {
+    buf_.resize(sizeof(f));
+    new (&buf_[0]) F(f);
+    using FF = R (*)(Fun*, ArgTypes...);
+    m_.obj_ = (FF) ([](Fun* f, ArgTypes...args) {
+        return reinterpret_cast< F* >(&f->buf_[0])->operator()(args...);
+    });
 }
 
 template < typename R, typename...ArgTypes >
 R Fun< R (ArgTypes...) >::operator()(ArgTypes...args) {
-    return f_.free_(args...);
+   return m_.call_(this, args...);
 }
 
 template < typename R, typename...ArgTypes >
@@ -80,6 +106,7 @@ template < typename T >
 R Fun< R (ArgTypes...) >::operator()(const T& obj, ArgTypes...args) {
     return f_.cmethod_(&obj, m_.cmethod_, args...);
 }
+
 
 //void return type
 template < typename...ArgTypes >
@@ -113,20 +140,16 @@ int Twice(int i) { return 2 * i; }
 
 int VoidParam() { return 2; }
 
-struct A {
-    int a;
-};
-
-struct B {
-    int b;
-};
-
-struct Class : A, virtual B {
+struct Class {
     int Twice(int i) { return 2 * i; }
     int VoidParam() { return 2; }
     int VoidParamConst() const { return 2; }
-    void Void() {}
+    void Void(int& i) {i = 1234;}
     Class() {}
+};
+
+struct Functor {
+    int operator()(int i) { return 3 * i; }
 };
 
 void test1 () {
@@ -134,14 +157,19 @@ void test1 () {
     const Class cc;
     Fun< int (int) > fun1(&Twice);  
     Fun< int (int) > fun2(&Class::Twice);
-    // Fun< int (   ) > fun3(&VoidParam);
-    // Fun< int (   ) > fun4(&Class::VoidParam);
-    // Fun< int (   ) > fun5(&Class::VoidParamConst);
-    // Fun< void(   ) > fun6(&Class::Void);
+    Fun< int (   ) > fun3(&VoidParam);
+    Fun< int (   ) > fun4(&Class::VoidParam);
+    Fun< int (   ) > fun5(&Class::VoidParamConst);
+    Fun< void(int&) > fun6(&Class::Void);
+    Fun< int(int) > fun7((Functor()));
    
-    // assert(fun1(2) == 4);
-    // assert(fun2(c, 3) == 6);
-    // assert(fun5(cc) == 2);
+    assert(fun1(2)    ==  4);
+    assert(fun2(c, 3) ==  6);
+    assert(fun5(cc)   ==  2);
+    assert(fun7(6)    == 18);
+    // int i = 0;
+    // fun6(c, i);
+    // assert(i == 1234);
     std::cout << "PASSED" << std::endl;
 }
 
