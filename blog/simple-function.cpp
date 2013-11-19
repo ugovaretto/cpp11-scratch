0da8632b39f8ca1f6278d4c0de7b697624ff555b
ugovaretto <uvaretto@cscs.ch>
2013-11-19 11:44:24 +0100

    Added void return type

--- blog/simple-function.cpp
+++ blog/simple-function.cpp
@@ -1,41 +1,42 @@
 //Author; Ugo Varetto
 //simple implementation of std::function-like class using lambdas and unions
-//TODO: void return type
+//TODO: assignment operator
 
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 //==============================================================================
 //generic, need this to be able to declare Fun< int(int) > instead of
 //Fun< int, int >
 template < typename > struct Fun {};
 
-//callable entities with a return type
+template < typename > struct FunBase {};
+
+
+
+
 template < typename R, typename...ArgTypes >
-struct Fun< R (ArgTypes...) >  {
+struct FunBase< R (ArgTypes...) > {
     struct Object {}; //generic object type used to cast member methods
     //constructors
-    Fun(const Fun& fun) : f_(fun.f_), m_(fun.m_) {
+    FunBase() {}
+    FunBase(const FunBase& fun) : f_(fun.f_), m_(fun.m_) {
         CopyObj(fun, *this);
     }
-    Fun(R (*f)(ArgTypes...));
-    template < typename T > Fun(R (T::*f)(ArgTypes...) );
-    template < typename T > Fun(R (T::*f)(ArgTypes...) const);
-    template < typename T > Fun(const T& f);
-    Fun(Fun&&) = default;
-    Fun& operator=(Fun f) {
+    FunBase(R (*f)(ArgTypes...));
+    template < typename T > FunBase(R (T::*f)(ArgTypes...) );
+    template < typename T > FunBase(R (T::*f)(ArgTypes...) const);
+    template < typename T > FunBase(const T& f);
+    FunBase(FunBase&&) = default;
+    FunBase& operator=(FunBase f) {
         Destruct(this);
         f_ = f.f_;
         m_ = f.m_;
         buf_ = std::move(f.buf_);
         return *this;
     }
-    //operator()
-    R operator()(ArgTypes...args) const;
-    template < typename T > R operator()(T& obj, ArgTypes...args) const;
-    template < typename T > R operator()(const T& obj, ArgTypes...args) const;
-
-    //delegate functions
+     //delegate functions
     union {
         R(*free_)(ArgTypes...);
         R(*method_)(void* ,  R (Object::*)(ArgTypes...), ArgTypes...);
@@ -47,35 +48,35 @@ struct Fun< R (ArgTypes...) >  {
     union {
         R(Object::*method_)(ArgTypes...);
         R(Object::*cmethod_)(ArgTypes...) const;
-        R(*call_)(const Fun*, ArgTypes...);
-        R(*obj_)(Fun*, ArgTypes...);
+        R(*call_)(const FunBase*, ArgTypes...);
+        R(*obj_)(FunBase*, ArgTypes...);
     } m_;
-    void (*Destruct)(Fun*) = nullptr;
-    void (*CopyObj)(const Fun&, Fun&) = nullptr;
+    void (*Destruct)(FunBase*) = nullptr;
+    void (*CopyObj)(const FunBase&, FunBase&) = nullptr;
     //storage for functor objects, in a real-world scenario we would need to
     //either pass a custom allocator using e.g. stack memory for small objects
     //or use a custom data type
     std::vector< char > buf_;
-    ~Fun() {
+    ~FunBase() {
         Destruct(this);
      }
 };
 
 //constructors
 template < typename R, typename...ArgTypes >
-Fun< R (ArgTypes...) >::Fun(R (*f)(ArgTypes...)) { 
+FunBase< R (ArgTypes...) >::FunBase(R (*f)(ArgTypes...)) { 
     f_.free_ = f; 
-    using T = R (*)(const Fun*, ArgTypes...);
-    m_.call_ = (T) ([](const Fun* f, ArgTypes...args) {
+    using T = R (*)(const FunBase*, ArgTypes...);
+    m_.call_ = (T) ([](const FunBase* f, ArgTypes...args) {
          return f->f_.free_(std::forward< ArgTypes >(args)...);
     });
-    Destruct = [](Fun*) {};
-    CopyObj  = [](const Fun&, Fun&) {};
+    Destruct = [](FunBase*) {};
+    CopyObj  = [](const FunBase&, FunBase&) {};
 }
 
 template < typename R, typename...ArgTypes >
 template < typename T >
-Fun< R(ArgTypes...) >::Fun(R (T::*f) (ArgTypes...) ) {
+FunBase< R(ArgTypes...) >::FunBase(R (T::*f) (ArgTypes...) ) {
     using F = R (*)(void*, R (Object::* )(ArgTypes...), ArgTypes...);
     using OM = R (Object::*)(ArgTypes...);
     f_.method_ = (F) ([](void* obj, OM m, ArgTypes...args) {
@@ -87,13 +88,13 @@ Fun< R(ArgTypes...) >::Fun(R (T::*f) (ArgTypes...) ) {
          });
     using OM = R (Object::*)(ArgTypes...);
     m_.method_ = reinterpret_cast< OM >(f); 
-    Destruct = [](Fun*) {};
-    CopyObj  = [](const Fun&, Fun&) {};
+    Destruct = [](FunBase*) {};
+    CopyObj  = [](const FunBase&, FunBase&) {};
 }
 
 template < typename R, typename...ArgTypes >
 template < typename T >
-Fun< R(ArgTypes...) >::Fun(R (T::*f) (ArgTypes...) const) {
+FunBase< R(ArgTypes...) >::FunBase(R (T::*f) (ArgTypes...) const) {
     using F = R (*)(const void*, R (Object::* )(ArgTypes...) const,
               ArgTypes...);
     using OM = R (Object::*)(ArgTypes...) const;
@@ -105,80 +106,111 @@ Fun< R(ArgTypes...) >::Fun(R (T::*f) (ArgTypes...) const) {
         });
     using OM = R (Object::*)(ArgTypes...) const;
     m_.cmethod_ = reinterpret_cast< OM >(f);
-    Destruct = [](Fun*) {}; 
-    CopyObj  = [](const Fun&, Fun&) {};
+    Destruct = [](FunBase*) {}; 
+    CopyObj  = [](const FunBase&, FunBase&) {};
 }
 
 template < typename R, typename...ArgTypes >
 template < typename F >
-Fun< R (ArgTypes...) >::Fun(const F& f) {
+FunBase< R (ArgTypes...) >::FunBase(const F& f) {
     buf_.resize(sizeof(f));
     new (&buf_[0]) F(f);
-    using FF = R (*)(Fun*, ArgTypes...);
-    m_.obj_ = (FF) ([](Fun* f, ArgTypes...args) {
+    using FF = R (*)(FunBase*, ArgTypes...);
+    m_.obj_ = (FF) ([](FunBase* f, ArgTypes...args) {
         return reinterpret_cast< F* >(&f->buf_[0])
                                 ->operator()(std::forward< ArgTypes >(args)...);
     });
-    Destruct = [](Fun* f) {
+    Destruct = [](FunBase* f) {
         F* fun = reinterpret_cast< F* >(&f->buf_[0]);
         fun->F::~F();
     };
-    CopyObj  = [](const Fun& src, Fun& target) {
+    CopyObj  = [](const FunBase& src, FunBase& target) {
         target.buf_.resize(src.buf_.size());
-        new (&target.buf_[0]) Fun(src);
+        new (&target.buf_[0]) FunBase(src);
     };
 }
 
-//operator()
+//callable entities with a return type
+template < typename R, typename...ArgTypes >
+struct Fun< R (ArgTypes...) > : FunBase< R (ArgTypes...) > {
+    using Base = FunBase< R(ArgTypes...) >;
+    Fun() : Base([](ArgTypes...) { 
+        throw std::logic_error("EMPTY FUNCTION OBJECT");
+        return R();
+    }){}
+    Fun(const Fun& f) : Base(f) {}
+    Fun(Fun&& f) :  Base(f) {} 
+    Fun(R (*f)(ArgTypes...)) :  Base(f) {}
+    template < typename T > Fun(R (T::*f)(ArgTypes...) ) : Base(f) {}
+    template < typename T > Fun(R (T::*f)(ArgTypes...) const) : Base(f) {}
+    template < typename T > Fun(const T& f) : Base(f) {}
+    Fun& operator=(Fun f) {
+        Base::operator=(f);
+        return *this;
+    }    
+    //operator()
+    R operator()(ArgTypes...args) const;
+    template < typename T > R operator()(T& obj, ArgTypes...args) const;
+    template < typename T > R operator()(const T& obj, ArgTypes...args) const; 
+};
+
 template < typename R, typename...ArgTypes >
 R Fun< R (ArgTypes...) >::operator()(ArgTypes...args) const {
-   return m_.call_(this, args...);
+   return Base::m_.call_(this, args...);
 }
 
 template < typename R, typename...ArgTypes >
 template < typename T >
 R Fun< R (ArgTypes...) >::operator()(T& obj, ArgTypes...args) const {
-    return f_.method_(&obj, m_.method_, args...);
+    return Base::f_.method_(&obj, Base::m_.method_, args...);
 }
 
 template < typename R, typename...ArgTypes >
 template < typename T >
 R Fun< R (ArgTypes...) >::operator()(const T& obj, ArgTypes...args) const {
-    return f_.cmethod_(&obj, m_.cmethod_, args...);
+    return Base::f_.cmethod_(&obj, Base::m_.cmethod_, args...);
 }
 
-
-//TODO: callable entities without a return value (a.k.a void return type)
-//instead of rewriting a complete specialzition it is actually possible
-//to move the constructors and unions into a base class or use the
-//Barton - Nackman trick:
-//Fun< R(ArgTypes...) > : DelegateImpl< Fun< R, ArgTypes...> > {...}
-//DelegateImpl adds the required operator()(...) implementations and
-//has two specizations for void and non-void return type
+//callable entities with void return type
 template < typename...ArgTypes >
-struct Fun< void (ArgTypes...) > {
-    Fun(void (*f)(ArgTypes...));
-    template < typename T > Fun(void (T::*f)(ArgTypes...) );
-    template < typename T > Fun(void (T::*f)(ArgTypes...) const);
-
-    void (*f_)(ArgTypes...);  
+struct Fun< void (ArgTypes...) > : FunBase< void (ArgTypes...) > {
+    using Base = FunBase< void (ArgTypes...) >;
+    Fun() : Base([](ArgTypes...) { 
+        throw std::logic_error("EMPTY FUNCTION OBJECT");
+    }){}
+    Fun(const Fun& f) : Base(f) {}
+    Fun(Fun&& f) :  Base(f) {} 
+    Fun(void (*f)(ArgTypes...)) :  Base(f) {}
+    template < typename T > Fun(void (T::*f)(ArgTypes...) ) : Base(f) {}
+    template < typename T > Fun(void (T::*f)(ArgTypes...) const) : Base(f) {}
+    template < typename T > Fun(const T& f) : Base(f) {}
+    Fun& operator=(Fun f) {
+        Base::operator=(f);
+        return *this;
+    }    
+    //operator()
+    void operator()(ArgTypes...args) const;
+    template < typename T > void operator()(T& obj, ArgTypes...args) const;
+    template < typename T > 
+                      void operator()(const T& obj, ArgTypes...args) const; 
 };
 
 template < typename...ArgTypes >
-Fun< void (ArgTypes...) >::Fun(void (*f)(ArgTypes...)) { 
-    std::cout << "void (*f)(ArgTypes...)" << std::endl;
+void Fun< void (ArgTypes...) >::operator()(ArgTypes...args) const {
+    Base::m_.call_(this, args...);
 }
 
 template < typename...ArgTypes >
 template < typename T >
-Fun< void(ArgTypes...) >::Fun(void (T::*f) (ArgTypes...) ) {
-    std::cout << "void (T::*f)(ArgTypes...)" << std::endl;
+void Fun< void (ArgTypes...) >::operator()(T& obj, ArgTypes...args) const {
+    Base::f_.method_(&obj, Base::m_.method_, args...);
 }
 
 template < typename...ArgTypes >
 template < typename T >
-Fun< void (ArgTypes...) >::Fun(void (T::*f) (ArgTypes...) const) {
-    std::cout << "void (T::*f)(ArgTypes...) const" << std::endl;
+void Fun< void (ArgTypes...) >::operator()(const T& obj, ArgTypes...args) 
+const {
+    Base::f_.cmethod_(&obj, Base::m_.cmethod_, args...);
 }
 
 //------------------------------------------------------------------------------
@@ -190,9 +222,8 @@ struct Class {
     int Twice(int i) { return 2 * i; }
     int VoidParam() { return 2; }
     int VoidParamConst() const { return 2; }
-    void Void(int& i) {i = 1234;}
+    void Void(int& i) const {i = 1234;}
     Class() {}
-    ~Class() {std::cout << "Class::~Class\n";}
 };
 
 struct Functor {
@@ -206,6 +237,7 @@ Fun< int(int) > RetFun() {
 void test1 () {
     Class c;
     const Class cc;
+    Fun< int (int) >   fun0;
     Fun< int (int) >   fun1(&Twice);  
     Fun< int (int) >   fun2(&Class::Twice);
     Fun< int () >      fun3(&VoidParam);
@@ -214,15 +246,22 @@ void test1 () {
     Fun< void(int&) >  fun6(&Class::Void);
     Fun< int (int) >   fun7((Functor()));
     auto               fun8(RetFun());
-   
+
+    try { 
+        fun0(9); //must throw
+        assert(false); 
+    } catch(...) {}
+    //fun0 = fun2;
+    //assert(fun0(2)    == fun1(2));
     assert(fun1(2)    ==  4);
     assert(fun2(c, 3) ==  6);
     assert(fun5(cc)   ==  2);
     assert(fun7(6)    == 18);
     assert(fun8(7)    == 21);
-    // int i = 0;
-    // fun6(c, i);
-    // assert(i == 1234);
+    //void return type
+    int i = 0;
+    fun6(c, i);
+    assert(i == 1234);
 }
 
 //------------------------------------------------------------------------------
